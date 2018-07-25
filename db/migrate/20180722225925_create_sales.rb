aac60be3188b00a5eca922266503fb3bd7ceee6a
wali89 <wali.rahman89@yahoo.com>
2018-07-25 06:53:03 -0400

    changes to sales migration

--- db/migrate/20180722225925_create_sales.rb
+++ db/migrate/20180722225925_create_sales.rb
@@ -1,9 +1,7 @@
 class CreateSales < ActiveRecord::Migration[5.2]
   def change
     create_table :sales do |t|
-      t.string :name
       t.integer :price
-      t.text :description
       t.belongs_to :user, foreign_key: true
       t.belongs_to :item, foreign_key: true
       
