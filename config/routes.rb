d8b5f86f92ed5437732d4d00c7e87aa6ab1fcdb0
wali89 <wali.rahman89@yahoo.com>
2018-07-27 17:27:33 -0400

    minor fixes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get '/users', to:'users#index', as: 'users'
   post '/users', to:'users#create'
   get '/users/:id', to: 'users#show', as: 'user'
-  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
 
   get '/signin', to: 'session#new', as: 'signin'
   post '/session', to: 'session#create', as: 'session'
