e8eb6d1b35dd794b7262583dcf24ff5cb951c1a2
wali89 <wali.rahman89@yahoo.com>
2018-07-30 16:36:38 -0400

    update to models and migration to add deals and stores

--- db/migrate/20180722225925_create_sales.rb
+++ db/migrate/20180722225925_create_sales.rb
@@ -1,7 +1,6 @@
 class CreateSales < ActiveRecord::Migration[5.2]
   def change
     create_table :sales do |t|
-      t.integer :price
       t.belongs_to :user, foreign_key: true
       t.belongs_to :deal, foreign_key: true
 
