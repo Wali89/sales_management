46e6918bc81bbdd26767bc824b6fac959f4dd1d2
wali89 <wali.rahman89@yahoo.com>
2018-08-01 14:12:24 -0400

    added amount to sales table

--- db/migrate/20180722225925_create_sales.rb
+++ db/migrate/20180722225925_create_sales.rb
@@ -1,6 +1,7 @@
 class CreateSales < ActiveRecord::Migration[5.2]
   def change
     create_table :sales do |t|
+      t.integer :amount
       t.belongs_to :user, foreign_key: true
       t.belongs_to :deal, foreign_key: true
 
