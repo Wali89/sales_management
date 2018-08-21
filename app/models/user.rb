6ce2a6125550a308a8e657c147e171eaf3b3ea17
wali89 <wali.rahman89@yahoo.com>
2018-08-21 07:46:51 -0400

    8-21-18 minor updates to forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true
   validates :email, presence: true
+  validates :email, uniqueness: true
   validates :password, presence: true
 
   def deal_count
